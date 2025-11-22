4fd9b1d76cd59d3970570ace95e00d7350589838
Karthi <Karthicoc7075@gmail.com>
2025-11-22 14:14:58 +0530

    initial commit

---
+++ _next/static/Cl9DbDAILaZo80m8tvI7h/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/b9dae63a5abce9f6.js"
+  ],
+  "/_error": [
+    "static/chunks/d0add15dbe92d321.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/b9dae63a5abce9f6.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/73f69c711baedc71.js",
+  "static/chunks/3ecc685019add803.js",
+  "static/chunks/7a225302aa43d2b9.js",
+  "static/chunks/turbopack-5aef2de0cf8da419.js"
+])
