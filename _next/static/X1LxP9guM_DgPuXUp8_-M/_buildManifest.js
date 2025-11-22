0714bed4ec9a4df91d8ac60295d2df11dc8a14be
Karthi <Karthicoc7075@gmail.com>
2025-11-22 07:56:53 +0530

    Updates

---
+++ _next/static/X1LxP9guM_DgPuXUp8_-M/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/ad88776a175cb119.js"
+  ],
+  "/_error": [
+    "static/chunks/9da2309089069798.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/4e4850f6b52613cd.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/c30b9fb5dd702d17.js",
+  "static/chunks/a62efb0a3401031d.js",
+  "static/chunks/6b8eb81b95ec4ac9.css",
+  "static/chunks/turbopack-22c48ba67bdd1962.js"
+])
