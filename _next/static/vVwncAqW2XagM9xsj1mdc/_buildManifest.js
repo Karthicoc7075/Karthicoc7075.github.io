dc81918ba35dbd432f4c406c32619b68d693bd05
Karthi <Karthicoc7075@gmail.com>
2025-12-19 19:31:27 +0530

    Initial Project

---
+++ _next/static/vVwncAqW2XagM9xsj1mdc/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/47d356d5a62b1d0b.js"
+  ],
+  "/_error": [
+    "static/chunks/d0add15dbe92d321.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
