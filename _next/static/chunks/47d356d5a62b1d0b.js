ea96fcdce6fe3482f04cf1ed72d2a784fcd9d8ef
Karthi <Karthicoc7075@gmail.com>
2025-12-19 19:46:32 +0530

    First commit

---
+++ _next/static/chunks/47d356d5a62b1d0b.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/73f69c711baedc71.js",
+  "static/chunks/3ecc685019add803.js",
+  "static/chunks/b209f232dd5a016c.js",
+  "static/chunks/turbopack-0a506bf4a522a0af.js"
+])
